fca1e28255ffd5ff4c3b1fa90dfaf5a2c54115e2
Alex Lev <levzx18@gmail.com>
2023-06-24 15:23:38 +0200

    feat(802.15.4): add pan ID to address

--- src/ieee802154/address.rs
+++ src/ieee802154/address.rs
@@ -1,5 +1,28 @@
 use crate::address::Address;
 
+/// The PAN ID is an ID used to identify a group of devices. An address must be
+/// associated with a PAN ID.
+#[cfg_attr(feature = "ufmt", derive(ufmt::derive::uDebug))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PanId(u16);
+
+impl const Address for PanId {
+    type Inner = u16;
+
+    fn broadcast() -> Self {
+        PanId(0xFFFF)
+    }
+    fn value(&self) -> Self::Inner {
+        self.0
+    }
+}
+
+impl PanId {
+    pub const fn new(address: u16) -> Self {
+        PanId(address)
+    }
+}
+
 #[cfg_attr(feature = "ufmt", derive(ufmt::derive::uDebug))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AddressKind {
@@ -9,20 +32,30 @@ pub enum AddressKind {
 
 #[cfg_attr(feature = "ufmt", derive(ufmt::derive::uDebug))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct ShortAddress(u8);
+pub struct ShortAddress(PanId, u16);
 
 #[cfg_attr(feature = "ufmt", derive(ufmt::derive::uDebug))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct LongAddress(u64);
+pub struct LongAddress(PanId, u64);
 
 impl const Address for ShortAddress {
-    type Inner = u8;
+    type Inner = u16;
 
     fn broadcast() -> Self {
-        ShortAddress(0xFF)
+        ShortAddress(PanId::broadcast(), 0xFFFF)
     }
 
     fn value(&self) -> Self::Inner {
+        self.1
+    }
+}
+
+impl ShortAddress {
+    pub const fn new(pan: PanId, address: u16) -> Self {
+        ShortAddress(pan, address)
+    }
+
+    pub const fn pan_id(&self) -> PanId {
         self.0
     }
 }
@@ -31,10 +64,20 @@ impl const Address for LongAddress {
     type Inner = u64;
 
     fn broadcast() -> Self {
-        LongAddress(0xFFFFFFFF)
+        LongAddress(PanId::broadcast(), 0xFFFFFFFF)
     }
 
     fn value(&self) -> Self::Inner {
+        self.1
+    }
+}
+
+impl LongAddress {
+    pub const fn new(pan: PanId, address: u64) -> Self {
+        LongAddress(pan, address)
+    }
+
+    pub const fn pan_id(&self) -> PanId {
         self.0
     }
 }
@@ -43,16 +86,18 @@ impl const Address for LongAddress {
 mod tests {
     use crate::{
         address::Address,
-        ieee802154::address::{LongAddress, ShortAddress},
+        ieee802154::address::{LongAddress, PanId, ShortAddress},
     };
 
     #[test]
     fn broadcast_for_short_address() {
-        assert_eq!(ShortAddress::broadcast().value(), 0xFF)
+        assert_eq!(ShortAddress::broadcast().value(), 0xFFFF);
+        assert_eq!(ShortAddress::broadcast().pan_id(), PanId::broadcast());
     }
 
     #[test]
     fn broadcast_for_long_address() {
-        assert_eq!(LongAddress::broadcast().value(), 0xFFFFFFFF)
+        assert_eq!(LongAddress::broadcast().value(), 0xFFFFFFFF);
+        assert_eq!(LongAddress::broadcast().pan_id(), PanId::broadcast());
     }
 }
